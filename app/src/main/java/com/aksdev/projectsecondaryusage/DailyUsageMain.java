5a727d08d58cc86abf1409462bcf12dc52484fec
ahmetkaansever <ahmetsever2003@yahoo.com>
2022-05-08 18:17:30 +0300

    Merging

--- app/src/main/java/com/aksdev/projectsecondaryusage/DailyUsageMain.java
+++ app/src/main/java/com/aksdev/projectsecondaryusage/DailyUsageMain.java
@@ -4,17 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DailyUsageMain extends AppCompatActivity {
 
     Button button;
+    TextView overallDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_primary_usage);
+
+        overallDisplay = findViewById(R.id.overallDisplayed);
 
         Button buttonPrimaryUsage = findViewById(R.id.buttonPrimaryUsage);
         Button buttonSecondaryUsage = findViewById(R.id.buttonSecondaryUsage);
